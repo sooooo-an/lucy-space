de24433b1e85324bdd0eca18fb037f971f67d11b
sooooo-an <devsoo0527@gmail.com>
2024-10-22 18:42:33 +0900

    feat: redirect route

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/posts",
+        permanent: true,
+      },
+      {
+        source: "/projects",
+        destination: "/projects/developers-store",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
