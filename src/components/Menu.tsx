d80ca2d89b61266098175ed30c32d695aaa0effb
sooooo-an <devsoo0527@gmail.com>
2024-10-24 12:04:41 +0900

    fix: remove home menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -4,16 +4,14 @@ import Link from "next/link";
 interface MenuType {
   name: string;
   href: string;
+  target?: string;
 }
 
 const MENUS: MenuType[] = [
   {
-    name: "Home",
-    href: "/",
-  },
-  {
     name: "About",
-    href: "/about",
+    href: "https://sooooan.notion.site/About-111a7c38fabd805187f2f21159b234a9?pvs=74",
+    target: "_blank",
   },
   {
     name: "Posts",
@@ -29,9 +27,11 @@ export default function Menu() {
   return (
     <nav>
       <ul className="flex gap-4 font-bold">
-        {MENUS.map((menu) => (
-          <li key={menu.href}>
-            <Link href={menu.href}>{menu.name}</Link>
+        {MENUS.map(({ href, name, target }) => (
+          <li key={href}>
+            <Link href={href} target={target ? target : "_self"}>
+              {name}
+            </Link>
           </li>
         ))}
       </ul>
