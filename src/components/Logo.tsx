e503510841a78846536658dafc60ccfa852952bb
sooooo-an <devsoo0527@gmail.com>
2024-10-28 15:21:59 +0900

    feat: set logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Logo() {
   return (
     <Link href="/">
-      <h1 className="font-bold text-2xl">Lucy.Space.</h1>
+      <h1 className="font-bold text-2xl flex items-center gap-1">
+        <Image src="/images/logo.png" alt="Lucy.Space" width={40} height={40} />
+        Lucy.Space.
+      </h1>
     </Link>
   );
 }
