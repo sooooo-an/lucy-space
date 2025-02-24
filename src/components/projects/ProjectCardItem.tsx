5d3127612e82a287134d2524e1d509321e4792c6
sooooo-an <devsoo0527@gmail.com>
2025-02-24 15:05:47 +0900

    fix: test

--- src/components/projects/ProjectCardItem.tsx
+++ src/components/projects/ProjectCardItem.tsx
@@ -15,9 +15,7 @@ export default function ProjectCardItem({ item }: Props) {
   const { path, github, thumbnail, title, url, description } = item
   return (
     <div className="rounded-lg border-border bg-prj-blue p-2 shadow-md">
-      <Link href={`projects/${path}`}>
-        <ProjectCardImage thumbnail={thumbnail} />
-      </Link>
+      <Link href={`projects/${path}`}>{/* <ProjectCardImage thumbnail={thumbnail} /> */}</Link>
       <ProjectCardContent github={github} title={title} url={url} description={description} />
     </div>
   )
