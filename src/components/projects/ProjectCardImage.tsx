7852180fd6d3ef00fb3987b3cc5c4acb716294b9
sooooo-an <devsoo0527@gmail.com>
2025-02-24 12:17:18 +0900

    fix: test

--- src/components/projects/ProjectCardImage.tsx
+++ src/components/projects/ProjectCardImage.tsx
@@ -1,28 +1,19 @@
-'use client'
-
 import Image from 'next/image'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 type Props = {
   thumbnail: string
 }
 
 export default function ProjectCardImage({ thumbnail }: Props) {
-  const [isLoading, setIsLoading] = useState(true)
   return (
     <div className="relative overflow-hidden">
-      <div
-        className={`absolute inset-0 transition-opacity duration-500 ${isLoading ? 'opacity-100' : 'opacity-0'}`}
-      >
-        <div className="h-64 w-full animate-pulse rounded-lg bg-gray-300"></div>
-      </div>
       <Image
         src={thumbnail}
         width={250}
         height={200}
         alt="project-thumbnail"
-        className={`h-64 w-full transition-opacity duration-500 ${isLoading ? 'opacity-0' : 'opacity-100'}`}
-        onLoad={() => setIsLoading(false)}
+        className="h-64 w-full rounded-tl-lg rounded-tr-lg transition-transform duration-300 ease-in-out hover:scale-110"
         priority
       />
     </div>
