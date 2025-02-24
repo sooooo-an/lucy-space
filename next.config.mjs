303ad531d4b94f49d1bfd1a6034fd181302c6fa3
sooooo-an <devsoo0527@gmail.com>
2025-02-24 09:36:34 +0900

    refactor: Updated Image cache

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    minimumCacheTTL: 60 * 60 * 24,
+    formats: ['image/webp'],
     remotePatterns: [
       {
         hostname: 'res.cloudinary.com',
