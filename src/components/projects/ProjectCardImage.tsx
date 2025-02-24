c87707407fb3fa921b99ec06e720410305156fb0
sooooo-an <devsoo0527@gmail.com>
2025-02-24 12:04:36 +0900

    fix: CLS

--- src/components/projects/ProjectCardImage.tsx
+++ src/components/projects/ProjectCardImage.tsx
@@ -11,13 +11,17 @@ export default function ProjectCardImage({ thumbnail }: Props) {
   const [isLoading, setIsLoading] = useState(true)
   return (
     <div className="relative overflow-hidden">
-      {isLoading && <div className="absolute inset-0 animate-pulse rounded-lg bg-gray-300"></div>}
+      <div
+        className={`absolute inset-0 transition-opacity duration-500 ${isLoading ? 'opacity-100' : 'opacity-0'}`}
+      >
+        <div className="h-64 w-full animate-pulse rounded-lg bg-gray-300"></div>
+      </div>
       <Image
         src={thumbnail}
         width={250}
         height={200}
         alt="project-thumbnail"
-        className="w-full rounded-tl-lg rounded-tr-lg transition-transform duration-300 ease-in-out hover:scale-110"
+        className={`h-64 w-full transition-opacity duration-500 ${isLoading ? 'opacity-0' : 'opacity-100'}`}
         onLoad={() => setIsLoading(false)}
         priority
       />
