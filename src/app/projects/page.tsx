88f91ceaa6dcdd89f8df2f366ad3ae7165d8342d
sooooo-an <devsoo0527@gmail.com>
2025-02-24 15:10:07 +0900

    fix: test

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,15 +5,15 @@ export const revalidate = 86400
 
 export default async function page() {
   const projects = await getProject(process.env.NOTION_PROJECT_DATABASE_ID!)
-  const list = projects.map((item) => ({
-    ...item,
-    thumbnail: `/api/image-proxy?url=${encodeURIComponent(item.thumbnail)}`,
-  }))
+  // const list = projects.map((item) => ({
+  //   ...item,
+  //   thumbnail: `/api/image-proxy?url=${encodeURIComponent(item.thumbnail)}`,
+  // }))
 
   return (
     <main className="pt-5">
       <div className="container mx-auto mb-5 px-4 md:px-0">
-        <ProjectList list={list} />
+        <ProjectList list={projects} />
       </div>
     </main>
   )
